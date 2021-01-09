2f015dba6551312a67ed22583651818937db8a8d
Ron Cagle <ron@roncagle.com>
2021-01-09 17:16:01 -0500

    Code from class at stopping point

--- handlers/home.js
+++ handlers/home.js
@@ -30,6 +30,56 @@ module.exports = (req, res) => {
       res.end();
     });
 
+  } else if (pathname === '/cats/add-cat' && req.method === 'GET') {
+    let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
+
+    const index = fs.createReadStream(filePath);
+
+    index.on('data', (data) => {
+      res.write(data);
+    });
+    index.on('end', () => {
+      res.end();
+    })
+    index.on('error', (err) => {
+      console.log(err);
+    })
+
+  } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
+    let filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
+
+    const index = fs.createReadStream(filePath);
+
+    index.on('data', (data) => {
+      res.write(data);
+    });
+    index.on('end', () => {
+      res.end();
+    })
+    index.on('error', (err) => {
+      console.log(err);
+    })
+  } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
+
+    const index = fs.createReadStream(filePath);
+
+    index.on('data', (data) => {
+      res.write(data);
+    });
+    index.on('end', () => {
+      res.end();
+    })
+    index.on('error', (err) => {
+      console.log(err);
+    })
+
+  } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
+    let formData = "";
+    req.on('data', (data) => {
+      console.log("the breed form data is ", data);
+      formData += data;
+      console.log("the new data is ", formData)
+    })
   } else {
     return true;
   }
